ec25181af1bfcb05c715cde4d7043c20352f8aaa
mina-yamakawa <st2117a@gmail.com>
2026-03-26 19:49:11 +0900

    fix app run

--- app.py
+++ app.py
@@ -704,4 +704,4 @@ def update_settlement_status():
     return redirect(url_for('settlements', month=month))
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
